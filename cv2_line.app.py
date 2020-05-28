74e3c0051bf689e80a5126c7c3bd9c015d353588
zer0 <osom8979@gmail.com>
2020-05-28 21:31:32 +0900

    Fixed script errors.

--- cv2_line.app.py
+++ cv2_line.app.py
@@ -3,13 +3,10 @@
 import numpy as np
 import cv2
 
-LINE_4_NAME = '4'
-LINE_8_NAME = '8'
-LINE_AA_NAME = 'AA'
 LINE_NAME_TO_NUM = {
-    LINE_4_NAME: cv2.LINE_4,
-    LINE_8_NAME: cv2.LINE_8,
-    LINE_AA_NAME: cv2.LINE_AA
+    '4': cv2.LINE_4,
+    '8': cv2.LINE_8,
+    'AA': cv2.LINE_AA
 }
 LINE_NUM_TO_NAME = {v: k for k, v in LINE_NAME_TO_NUM.items()}
 
@@ -22,7 +19,8 @@ shift = 0
 def on_set(key, val):
     if key == 'color':
         global color
-        color = str(val).split(',')
+        h = str(val).lstrip('#')
+        color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
     elif key == 'thickness':
         global thickness
         thickness = int(val)
@@ -36,7 +34,7 @@ def on_set(key, val):
 
 def on_get(key):
     if key == 'color':
-        return ','.join(list(str(x) for x in color))
+        return ','.join(list(map(lambda x: str(x), color)))
     elif key == 'thickness':
         return str(thickness)
     elif key == 'line_type':
@@ -45,39 +43,44 @@ def on_get(key):
         return str(shift)
 
 
-def on_create():
-    return True
+def convert_drawable_line(line, w, h, is_absolute_coordinates=False):
+    if is_absolute_coordinates:
+        return int(line[0]), int(line[1])
+    else:
+        return int(line[0]*w), int(line[1]*h)
 
 
-def on_init():
-    return True
+def convert_drawable_lines(lines: np.ndarray, width, height):
+    is_absolute_coordinates = lines.dtype not in [np.float32, np.float64]
+    lines_rank = len(lines.shape)
+    result = list()
+    if lines_rank == 1:
+        assert lines.shape[0] >= 4
+        result.append(convert_drawable_line(lines[0:2], width, height, is_absolute_coordinates))
+    else:
+        assert lines_rank == 2
+        assert lines.shape[0] >= 1
+        assert lines.shape[1] >= 4
+        for i in range(lines.shape[0]):
+            result.append(convert_drawable_line(lines[i][0:2], width, height, is_absolute_coordinates))
+    return result
 
 
-def on_valid():
-    return True
-
-
-def on_run(source: np.ndarray, lines: np.ndarray):
+def on_run(source: np.ndarray, rectangles: np.ndarray):
     assert len(source.shape) == 3
     assert source.shape[0] >= 1
     assert source.shape[1] >= 1
     assert source.shape[2] >= 1
-
-    assert len(lines.shape) == 2
-    assert lines.shape[0] >= 1
-    assert lines.shape[1] == 4
-
     result = source.copy()
-    for i in range(lines.shape[0]):
+    for line in convert_drawable_lines(rectangles, source.shape[1], source.shape[0]):
         cv2.line(result,
-                 (int(lines[i][0]), int(lines[i][1])),
-                 (int(lines[i][2]), int(lines[i][2])),
-                 color, thickness, line_type, shift)
-    return {'result', result}
-
-
-def on_destroy():
-    return True
+                 (line[0], line[1]),
+                 (line[2], line[3]),
+                 color,
+                 thickness,
+                 line_type,
+                 shift)
+    return {'result': result}
 
 
 if __name__ == '__main__':
--- cv2_rectangle.app.py
+++ cv2_rectangle.app.py
@@ -2,15 +2,11 @@
 
 import numpy as np
 import cv2
-import sys
 
-LINE_4_NAME = '4'
-LINE_8_NAME = '8'
-LINE_AA_NAME = 'AA'
 LINE_NAME_TO_NUM = {
-    LINE_4_NAME: cv2.LINE_4,
-    LINE_8_NAME: cv2.LINE_8,
-    LINE_AA_NAME: cv2.LINE_AA
+    '4': cv2.LINE_4,
+    '8': cv2.LINE_8,
+    'AA': cv2.LINE_AA
 }
 LINE_NUM_TO_NAME = {v: k for k, v in LINE_NAME_TO_NUM.items()}
 
@@ -23,7 +19,8 @@ shift = 0
 def on_set(key, val):
     if key == 'color':
         global color
-        color = str(val).split(',')
+        h = str(val).lstrip('#')
+        color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
     elif key == 'thickness':
         global thickness
         thickness = int(val)
@@ -37,22 +34,37 @@ def on_set(key, val):
 
 def on_get(key):
     if key == 'color':
-        return ','.join(list(str(x) for x in color))
+        return ','.join(list(map(lambda x: str(x), color)))
     elif key == 'thickness':
         return str(thickness)
     elif key == 'line_type':
-        return LINE_NUMthickness
-
-def on_create():
-    return True
-
-
-def on_init():
-    return True
+        return LINE_NUM_TO_NAME[line_type]
+    elif key == 'shift':
+        return str(shift)
 
 
-def on_valid():
-    return True
+def convert_drawable_rect(r, w, h, is_absolute_coordinates=False):
+    if is_absolute_coordinates:
+        return int(r[0]), int(r[1]), int(r[2]), int(r[3])
+    else:
+        return int(r[0]*w), int(r[1]*h), int(r[2]*w), int(r[3]*h)
+
+
+def convert_drawable_rects(rectangles: np.ndarray, width, height):
+    is_absolute_coordinates = rectangles.dtype not in [np.float32, np.float64]
+    rectangles_rank = len(rectangles.shape)
+
+    result = list()
+    if rectangles_rank == 1:
+        assert rectangles.shape[0] >= 4
+        result.append(convert_drawable_rect(rectangles[0:4], width, height, is_absolute_coordinates))
+    else:
+        assert rectangles_rank == 2
+        assert rectangles.shape[0] >= 1
+        assert rectangles.shape[1] >= 4
+        for i in range(rectangles.shape[0]):
+            result.append(convert_drawable_rect(rectangles[i][0:4], width, height, is_absolute_coordinates))
+    return result
 
 
 def on_run(source: np.ndarray, rectangles: np.ndarray):
@@ -60,30 +72,16 @@ def on_run(source: np.ndarray, rectangles: np.ndarray):
     assert source.shape[0] >= 1
     assert source.shape[1] >= 1
     assert source.shape[2] >= 1
-
-    assert len(rectangles.shape) == 2
-    assert rectangles.shape[0] >= 1
-    assert rectangles.shape[1] == 4
-
     result = source.copy()
-
-    # sys.stdout.write(f"color : {color}")
-    # sys.stdout.write(f"thickness : {thickness}")
-    # sys.stdout.write(f"line_type : {line_type}")
-    # sys.stdout.flush()
-
-    for i in range(rectangles.shape[0]):
+    for rect in convert_drawable_rects(rectangles, source.shape[1], source.shape[0]):
         cv2.rectangle(result,
-                      (int(rectangles[i][0]), int(rectangles[i][1])),
-                      (int(rectangles[i][2]), int(rectangles[i][3])),
-                      (255, 255, 255),
+                      (rect[0], rect[1]),
+                      (rect[2], rect[3]),
+                      color,
                       thickness,
-                      line_type)
+                      line_type,
+                      shift)
     return {'result': result}
-
-
-def on_destroy():
-    return True
 
 
 if __name__ == '__main__':
