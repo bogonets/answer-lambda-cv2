a5878436fbaa045ef6279fd1f8fa2bcf5fdce7cb
zer0 <osom8979@gmail.com>
2020-08-20 17:02:08 +0900

    Download test asset: lenna.jpg

---
+++ tester.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import importlib.util
+import unittest
+
+
+def import_module(name):
+    spec = importlib.util.spec_from_file_location(name, f'{name}.app.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+cv2_orb = import_module('cv2_orb')
+
+
+class TestNumpyLambdas(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_orb(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
