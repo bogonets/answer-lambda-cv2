3e5c130202413887a8677c923fb8b85e11bdc2f4
zer0 <osom8979@gmail.com>
2020-08-31 15:47:14 +0900

    Checked the size of the numpy array in cv2_draw_matches_knn lambda.

--- cv2_draw_matches_knn.app.py
+++ cv2_draw_matches_knn.app.py
@@ -29,23 +29,25 @@ def on_get(key):
 def array_to_keypoints(array: np.ndarray) -> List[cv2.KeyPoint]:
     result = []
     for x in array:
-        result.append(cv2.KeyPoint(_angle=x[0],
-                                   _class_id=int(x[1]),
-                                   _octave=int(x[2]),
-                                   x=x[3],
-                                   y=x[4],
-                                   _response=x[5],
-                                   _size=x[6]))
+        if x.size >= 7:
+            result.append(cv2.KeyPoint(_angle=x[0],
+                                       _class_id=int(x[1]),
+                                       _octave=int(x[2]),
+                                       x=x[3],
+                                       y=x[4],
+                                       _response=x[5],
+                                       _size=x[6]))
     return result
 
 
 def array_to_matches(array: np.ndarray) -> List[List[cv2.DMatch]]:
     result = []
     for x in array:
-        result.append([cv2.DMatch(_distance=x[0],
-                                  _imgIdx=int(x[1]),
-                                  _queryIdx=int(x[2]),
-                                  _trainIdx=int(x[3]))])
+        if x.size >= 4:
+            result.append([cv2.DMatch(_distance=x[0],
+                                      _imgIdx=int(x[1]),
+                                      _queryIdx=int(x[2]),
+                                      _trainIdx=int(x[3]))])
     return result
 
 
